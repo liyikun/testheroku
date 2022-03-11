35cb0340828673eb97ade73da5003696d0084da3
liyikun <liyikun.l@bytedance.com>
2022-03-11 16:50:14 +0800

    chore: add port

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: npm start
+web: export PORT=$PORT npm start 
--- index.js
+++ index.js
@@ -2,5 +2,5 @@ const http = require('http')
 
 
 http.createServer((req, res) => {
-    res.write('hello')
+    res.write('Hello TikTok!')
 }).listen(process.env.PORT || 3000)
