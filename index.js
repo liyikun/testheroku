b4916beec93e636b26ca52724485f8c2fb3e7527
liyikun <liyikun.l@bytedance.com>
2022-03-11 16:52:25 +0800

    chore: hello

--- index.js
+++ index.js
@@ -2,5 +2,7 @@ const http = require('http')
 
 
 http.createServer((req, res) => {
-    res.write('Hello TikTok!')
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello TikTok!')
 }).listen(process.env.PORT || 3000)
